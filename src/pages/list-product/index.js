0bc34e68bdc22edc5c79de9c0a9d5995d72db982
TienDoPT <domanhtien1999@gmail.com>
2023-03-07 21:44:56 +0700

    tiendm first commit

--- src/pages/list-product/index.js
+++ src/pages/list-product/index.js
@@ -1,7 +1,18 @@
+import ProductsFilter from "./components/ProductsFilter";
+
 function ListProduct() {
-    return ( 
-        <div>List Product</div>
-     );
+    return (
+        <div class="container text-center">
+            <div class="row">
+                <div class="col-3 bg-primary">
+                    <ProductsFilter />
+                </div>
+                <div class="col bg-secondary">
+                    2 of 3 (wider)
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default ListProduct;
