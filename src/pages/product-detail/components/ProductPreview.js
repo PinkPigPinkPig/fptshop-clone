9444e8c308a18feb545dd257e73b2fd22361c5a2
TienDoPT <domanhtien1999@gmail.com>
2023-04-17 21:49:49 +0700

    hehe

--- src/pages/product-detail/components/ProductPreview.js
+++ src/pages/product-detail/components/ProductPreview.js
@@ -1,27 +1,42 @@
 import React from "react";
 import phoneImg from "../../../assets/images/product-detail/product_img.webp";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useSelector } from "react-redux";
+import { productSelector } from "ReduxSaga/Product/ProductRedux";
 
 const ProductPreview = () => {
+  const { productDetail } = useSelector(productSelector)
+  const specification = productDetail?.specification
+
   return (
     <div className="col">
       <img src={phoneImg} alt="" className="mb-3 w-100" />
       <div className="d-flex flex-column bg-light p-3 ">
         <div className="mb-3">
           <FontAwesomeIcon icon="mobile" className="me-1" />
-          <span>6.7 inch, Super AMOLED, FHD+, 1080 x 2400 Pixels</span>
+          <span>
+            {`${specification?.screenSize}, ${specification?.screenTech}, ${specification?.resolution}`}
+          </span>
         </div>
         <div className="mb-3">
           <FontAwesomeIcon icon="camera" className="me-1" />
-          <span>108.0 MP + 12.0 MP + 5.0 MP + 5.0 MP</span>
+          {specification?.cameras?.filter(res => res.cameraType === 'FRONT')?.map((cam, i) => {
+            return (
+              <span key={i}>{`${i === 0 ? '' : ' + '} ${cam?.cameraResolution}`}</span>
+            )
+          })}
         </div>
         <div className="mb-3">
           <FontAwesomeIcon icon="camera-rotate" className="me-1" />
-          <span>32.0 MP</span>
+          {specification?.cameras?.filter(res => res.cameraType === 'BACK')?.map((cam, i) => {
+            return (
+              <span key={i}>{`${i === 0 ? '' : ' + '} ${cam?.cameraResolution}`}</span>
+            )
+          })}
         </div>
         <div className="mb-3">
           <FontAwesomeIcon icon="microchip" className="me-1" />
-          <span>Snapdragon 778G</span>
+          <span>{specification?.cpu}</span>
         </div>
         <div>
           <FontAwesomeIcon icon="floppy-disk" className="me-1" />
@@ -37,6 +52,9 @@ const ProductPreview = () => {
           color="#cb1c22"
         />
         <span>Giao hàng toàn quốc</span>
+      </div>
+      <div className="d-flex flex-column bg-light p-3 ">
+
       </div>
     </div>
   );
